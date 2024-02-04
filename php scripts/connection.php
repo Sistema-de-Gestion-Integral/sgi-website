ebbbfc5f454b5911a46d591af58fe7eec7427699
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-02-04 11:02:13 -0600

    mostrar error

--- php scripts/connection.php
+++ php scripts/connection.php
@@ -1,2 +1,5 @@
 <?php
 $connection = new mysqli("localhost", "sgi_db_user", "SGIDB24!!", "sgi-system");
+if ($connection->connect_error) {
+    die('Error : (' . $connection->connect_error . ')');
+}
