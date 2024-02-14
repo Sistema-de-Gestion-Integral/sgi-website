46a0a21cd15231a824df2eae98b37b65a896232c
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-02-14 12:53:59 -0600

    update host

--- rfidadmin/database.php
+++ rfidadmin/database.php
@@ -2,7 +2,7 @@
 	class Database
 	{
 		private static $dbName = 'nodemcu_rfid_iot_projects' ;
-		private static $dbHost = '127.0.0.1';
+		private static $dbHost = 'localhost';
 		private static $dbUsername = 'root24';
 		private static $dbUserPassword = '@IoT24!!';
 		 
